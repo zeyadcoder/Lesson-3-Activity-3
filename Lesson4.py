100cd11f41df651eb44264f13d3c954861fa5680
zeyadcoder <Zeyad.bawany@gmail.com>
2024-09-08 15:19:05 +0500

    work5

---
+++ Lesson4.py
@@ -0,0 +1,15 @@
+import random 
+import time 
+
+def Date(start_date, end_date):
+    random_generator = random.random()
+    Date_format = "%m/%d/%y"
+    start_time = time.mktime(time.strptime(start_date, Date_format))
+    end_time = time.mktime(time.strptime(end_date, Date_format))
+    random_time = start_time + random_generator * (end_time - start_time)
+    random_date = time.strftime(Date_format, time.localtime(random_time))
+    return random_date
+print(Date("4/3/24", "6/8/25")) 
+
+
+
